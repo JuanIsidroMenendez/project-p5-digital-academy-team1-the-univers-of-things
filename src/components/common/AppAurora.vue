92e8f6743bc3be76e333e99bbc3fc5ead0a56724
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-06-10 15:25:48 +0200

    feat: add AppAurora component template with three decorative blobs

--- src/components/common/AppAurora.vue
+++ src/components/common/AppAurora.vue
@@ -1,1 +1,11 @@
 <!-- Fondo aurora animado: 3 blobs decorativos con fps-aurora -->
+<script setup>
+</script>
+
+<template>
+    <div class="aurora" aria-hidden="true">
+        <div class="aurora__blob aurora__blob--purple"></div>
+        <div class="aurora__blob aurora__blob--cyan"></div>
+        <div class="aurora__blob aurora__blob--orange"></div>
+    </div>
+</template>
