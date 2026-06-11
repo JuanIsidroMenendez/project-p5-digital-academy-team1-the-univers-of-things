42afcf4d18681d5e7b2a6176d1cfcce6ea0e393b
iulian640 <iuliantim21@gmail.com>
2026-06-11 12:49:31 +0200

    feat: created getuserprofile and create user functions

--- src/api/user.service.js
+++ src/api/user.service.js
@@ -3,7 +3,19 @@ import { db } from './firebase.js'
 
 export async function createUserProfile(uid, email, username) {
     const userRef = doc(db, 'users', uid)
-    const userData = {uid, email, username, role: 'customer', fav: [], profileImg: 'default.png'}
-    
+    const userData = { uid, email, username, role: 'customer', fav: [], profileImg: 'default.png' }
+
     await setDoc(userRef, userData)
 }
+
+export async function getUserProfile(uid) {
+    const userRef = doc(db, 'users', uid)
+
+    const snapshot = await getDoc(userRef)
+
+    if (!snapshot.exists()) {
+        return null
+    }
+return snapshot.data()
+}
+
