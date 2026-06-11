2e6a9cd5afe88994761078d09ef65a958225ccf5
iulian640 <iuliantim21@gmail.com>
2026-06-11 14:58:23 +0200

    feat: page routing done

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,56 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+
     {
+      path: '/admindashboard',
+      name: 'AdminDashboard',
+      component: () => import('@/views/AdminDashboard.vue'),
+      meta: { requiresAdmin: true }
+    },
+      {
+      path: '/catalog',
+      name: 'CatalogPage',
+      component: () => import('@/views/CatalogPage.vue')
+    },
+      {
+      path: '/destacados',
+      name: 'DestacadosPage',
+      component: () => import('@/views/DestacadosPage.vue')
+    },
+      {
+      path: '/favorites',
+      name: 'FavoritesPage',
+      component: () => import('@/views/FavoritesPage.vue'),
+      meta: { requiresAuth: true }
+    },
+      {
+      path: '/detail/:id',
+      name: 'GameDetailPage',
+      component: () => import('@/views/GameDetailPage.vue')
+    },
+      {
       path: '/',
-      name: 'home',
+      name: 'HomePage',
       component: () => import('@/views/HomePage.vue')
-    }
+    },
+      {
+      path: '/login',
+      name: 'LoginPage',
+      component: () => import('@/views/LoginPage.vue')
+    },
+      {
+      path: '/register',
+      name: 'RegisterPage',
+      component: () => import('@/views/RegisterPage.vue')
+    },
+     {
+      path: '/userdashboard',
+      name: 'UserDashboard',
+      component: () => import('@/views/UserDashboard.vue'),
+      meta: { requiresAuth: true }
+    },
+
   ],
 })
 
