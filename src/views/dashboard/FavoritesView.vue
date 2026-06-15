d0e87dc32b05c5958b3909bd7490393af0e9fcd1
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-06-15 21:27:28 +0200

    style: add FavoritesView scoped styles

--- src/views/dashboard/FavoritesView.vue
+++ src/views/dashboard/FavoritesView.vue
@@ -14,4 +14,23 @@ import DashboardLayout from '@/layouts/DashboardLayout.vue'
 
 <style lang="scss" scoped>
 @use '@/assets/styles/base/variables' as *;
+
+.favorites-view {
+    &__title {
+        font-family: $font-display;
+        font-weight: 800;
+        font-size: 1.8rem;
+        color: var(--color-text);
+        margin-bottom: 1.5rem;
+
+        @media (min-width: $bp-tablet) {
+            font-size: 2rem;
+        }
+
+        @media (min-width: $bp-desktop) {
+            font-size: 2.5rem;
+            margin-bottom: 2rem;
+        }
+    }
+}
 </style>
