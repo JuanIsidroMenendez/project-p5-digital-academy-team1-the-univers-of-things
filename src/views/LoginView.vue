01877fe45e37f04b54f388f24d5bd78f43668298
iulian640 <iuliantim21@gmail.com>
2026-06-15 21:53:54 +0200

    feat: connect auth store to Firestore user profile and role

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,1 +1,11 @@
 <!-- Vista login: formulario de acceso con validacion -->
+
+
+<!-- poner despues del login-->
+await authStore.login(email, password)
+
+if (authStore.profile?.role === 'admin') {
+  router.push('/admin')
+} else {
+  router.push('/dashboard')
+}
