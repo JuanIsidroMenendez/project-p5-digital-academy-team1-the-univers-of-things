d5f3889648d6ed45c8a6cbe768fa065430494060
iulian640 <iuliantim21@gmail.com>
2026-06-15 09:54:28 +0200

    feat: build FeaturedHero component

---
+++ src/components/featured/FeaturedHero.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <div class="featured-hero">
+        <p class="hero-tag">⚡ Selección del equipo</p>
+        <h1 class="page-hero-title">Juegos Destacados</h1>
+        <h2 class="page-subtitle">Los títulos más impresionantes de nuestra colección, seleccinados a mano por el equipo de FPS</h2>
+    </div>    
+</template>
