38e1d7e6b44e91aabfc465c34e1531597b444337
Juan <juan.isidro.menendez@gmail.com>
2026-06-15 12:20:48 +0200

    refactor: update routes according to new folder structure

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,7 +1,9 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from 'firebase.js'
+import { auth } from '@/api/firebase'
+import { logoutUser } from '@/services/auth-service'
+
 export const useAuthStore = defineStore('auth', () => {
     const user = ref(null)
     const loading = ref(true)
