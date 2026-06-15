d2f736333239bf8c68403d43f8b7dbd969c7fab7
iulian640 <iuliantim21@gmail.com>
2026-06-15 12:28:29 +0200

    style: refine FeaturedMonthly card layout and tag styles

---
+++ src/components/featured/FeaturedMonthly.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="featured-monthly">
+    <span class="featured-monthly__tag">• DESTACADO DEL MES</span>
+    <h1 class="featured-monthly__title">Neon Odyssey</h1>
+    <p class="featured-monthly__descripcion">Sumérgete en Neo-Tokio 2087, una megalópolis orbital donde la realidad y el código se fusionan.</p>
+  </div>
+</template>
--- src/views/FeaturedView.vue
+++ src/views/FeaturedView.vue
@@ -2,11 +2,14 @@
 import MainLayout from '@/layouts/MainLayout.vue'
 import FeaturedHero from '@/components/featured/FeaturedHero.vue'
 import FeaturedGrid from '@/components/featured/FeaturedGrid.vue'
+import FeaturedMonthly from '@/components/featured/FeaturedMonthly.vue'
 </script>
 <template>
     <MainLayout>
         <FeaturedHero></FeaturedHero>
+        <FeaturedMonthly></FeaturedMonthly>
         <FeaturedGrid></FeaturedGrid>
+
     </MainLayout>
     </template>
 
