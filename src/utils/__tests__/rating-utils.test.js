5fb0ebc1e71371adcfb054482d6004a254325c8d
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-06-16 21:28:49 +0200

    chore: skip rating-utils tests until Sprint 2

--- src/utils/__tests__/rating-utils.test.js
+++ src/utils/__tests__/rating-utils.test.js
@@ -1,1 +1,5 @@
-// Tests unitarios de rating-utils.js - ciclo Red-Green-Refactor
+// Tests unitarios en Sprint 2 cuando StarRating.vue este lista
+
+import { describe } from 'vitest'
+
+describe.skip('rating-utils', () => { })
