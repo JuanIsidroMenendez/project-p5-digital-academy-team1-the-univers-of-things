1ed6bca54eb165a7c242e22c5e2894830795d430
iulian640 <iuliantim21@gmail.com>
2026-06-18 14:46:16 +0200

    feat: connect FeaturedView and FeaturedMonthly to featured store

--- src/components/featured/FeaturedMonthly.vue
+++ src/components/featured/FeaturedMonthly.vue
@@ -1,7 +1,22 @@
+<script setup>
+import { useFeaturedStore } from '@/stores/featured-store.js'
+import { computed } from 'vue'
+
+
+const featuredStore = useFeaturedStore()
+
+const gameMonth = computed(() => featuredStore.gameOfTheMonth)
+
+</script>
+
+
+
+
 <template>
-  <div class="featured-monthly">
+  <div class="featured-monthly" v-if="gameMonth">
     <span class="featured-monthly__tag">• DESTACADO DEL MES</span>
-    <h1 class="featured-monthly__title">Neon Odyssey</h1>
-    <p class="featured-monthly__descripcion">Sumérgete en Neo-Tokio 2087, una megalópolis orbital donde la realidad y el código se fusionan.</p>
+    <h1 class="featured-monthly__title">{{ gameMonth.title }}</h1>
+    <p class="featured-monthly__descripcion">{{ gameMonth.short_description }}</p>
+    <img :src="gameMonth.thumbnail" />
   </div>
 </template>
--- src/views/FeaturedView.vue
+++ src/views/FeaturedView.vue
@@ -1,8 +1,16 @@
 <script setup>
+import { onMounted } from 'vue'
 import MainLayout from '@/layouts/MainLayout.vue'
 import FeaturedHero from '@/components/featured/FeaturedHero.vue'
 import FeaturedGrid from '@/components/featured/FeaturedGrid.vue'
 import FeaturedMonthly from '@/components/featured/FeaturedMonthly.vue'
+import { useFeaturedStore } from '@/stores/featured-store.js'
+
+const featuredStore = useFeaturedStore()
+
+onMounted(() => {
+  featuredStore.fetchAll()
+})
 </script>
 <template>
     <MainLayout>
