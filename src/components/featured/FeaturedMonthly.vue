cb04e5bcf772660312df10bc4c6560228dc36db4
iulian640 <iuliantim21@gmail.com>
2026-06-19 09:33:56 +0200

    style: game of the month styled

--- src/components/featured/FeaturedMonthly.vue
+++ src/components/featured/FeaturedMonthly.vue
@@ -22,22 +22,3 @@ const gameMonth = computed(() => featuredStore.gameOfTheMonth)
     <img :src="gameMonth.thumbnail" class="featured-monthly__image"/>
   </div>
 </template>
-
-<style scoped lang="scss">
-
-.featured-monthly {
-  display: flex;
-  max-width: 900px;
-  margin: 0 auto;
-  flex-direction: row-reverse;
-  gap: 2rem;
-  align-items: center;
-
-  &__text {
-  }
-
-  &__image {
-    width: 300px;
-  }
-}
-</style>
